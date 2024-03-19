e1571dd13b93489bd13c72f16790f53207663676
Floriano <floriano.webdev@gmail.com>
2024-03-19 17:21:07 +0100

    backup

--- sources/server.c
+++ sources/server.c
@@ -6,36 +6,35 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 12:19:04 by falberti          #+#    #+#             */
-/*   Updated: 2024/03/19 16:54:48 by falberti         ###   ########.fr       */
+/*   Updated: 2024/03/19 17:19:50 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-static g_var	g_char;
+// static g_var	g_char;
 
-static void	read_bit(int signum)
-{
-	g_char.i = 0;
-	g_char.c += ((signum & 1) << g_char.i);
-	g_char.i++;
-	printf("%d", signum);
-	if (g_char.i == 7)
-	{
-		ft_printf("%c", g_char.c);
-		if (g_char.c == '\0')
-				ft_printf("\n");
-		g_char.i = 0;
-		g_char.c = 0;
-	}
-}
-
-// void	test(int i)
+// static void	read_bit(int signum)
 // {
-// 	i = 1;
-// 	ft_printf("test");
-// 	return ;
+// 	printf("%d", signum);
+// 	g_char.i = 0;
+// 	g_char.c += ((signum & 1) << g_char.i);
+// 	g_char.i++;
+// 	if (g_char.i == 7)
+// 	{
+// 		ft_printf("%c", g_char.c);
+// 		if (g_char.c == '\0')
+// 			ft_printf("\n");
+// 		g_char.i = 0;
+// 		g_char.c = 0;
+// 	}
 // }
+
+void	test(int i)
+{
+	printf("Test: %d", i);
+	return ;
+}
 
 int	main(void)
 {
@@ -43,8 +42,8 @@ int	main(void)
 	ft_printf("My Server PID is: %d\n", getpid());
 	while (1)
 	{
-		signal(SIGUSR2, read_bit);
-		signal(SIGUSR1, read_bit);
+		signal(SIGUSR2, test);
+		signal(SIGUSR1, test);
 		pause();
 	}
 	return (0);
